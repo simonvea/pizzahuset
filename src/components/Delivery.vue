ab6357a3c82257bfa8d6cbebc6e5e0f3caa5e431
Simon Opheim <simon.opheim@gmail.com>
2019-05-31 23:21:08 +0200

    fixed spelling mistake

--- src/components/Delivery.vue
+++ src/components/Delivery.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="card" style="width: 18rem;">
         <div class="card-body">
-            <h5 class="card-title">Leverings info</h5>     
+            <h5 class="card-title">Leveringsinfo</h5>     
             <p class="card-text">
                 Some quick example text to build on the card title and make up the bulk of the card's content.
             </p>
